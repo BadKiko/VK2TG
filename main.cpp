83e8b3381cc900c9f216a490e50b1e050c0c1732
Niko-PRO <nikitka-nikitos-popov@mail.ru>
2023-11-10 02:47:23 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -1,55 +1,41 @@
 #include <iostream>
 #include <cstdio>
 #include <tgbot/tgbot.h>
-#include <crow.h>
-#include <thread>
-#include <string>
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
-
-    std::thread t1([&]()
+    TgBot::Bot bot("6740633385:AAENDMXHCeoBcvv1lv9NL2OjIO53cEPgjd4");
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message)
     {
-        crow::SimpleApp app;
-
-        CROW_ROUTE(app, "/")([]()
-        {
-            return "Hello Host!";
-        });
-
-        app.port(8080).run_async();
+        bot.getApi().sendMessage(message->chat->id, "Hi, Penis!");
     });
-
-    std::thread t2([&]()
+    bot.getEvents().onCommand("suka", [&bot](TgBot::Message::Ptr message)
     {
-        TgBot::Bot bot("6740633385:AAENDMXHCeoBcvv1lv9NL2OjIO53cEPgjd4");
-
-        bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
-        {
-            //This
-        });
-
-        try
-        {
-            printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-            TgBot::TgLongPoll longPoll(bot);
-            while (true)
-            {
-                printf("Long poll started\n");
-                longPoll.start();
-            }
-        }
-        catch (TgBot::TgException& e)
-        {
-            printf("error: %s\n", e.what());
-        }
-
-        std::cout << "Test Text";
+        bot.getApi().sendMessage(message->chat->id, "Hahahaah! Such my cock, beach)))");
     });
-
-    t1.join();
-    t2.join();
-
+    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message)
+    {
+        printf("User wrote %s, ID: \n", message->text.c_str());
+        if (StringTools::startsWith(message->text, "/start"))
+        {
+            return;
+        }
+        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
+    });
+    try
+    {
+        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
+        TgBot::TgLongPoll longPoll(bot);
+        while (true)
+        {
+            printf("Long poll started\n");
+            longPoll.start();
+        }
+    }
+    catch (TgBot::TgException& e)
+    {
+        printf("error: %s\n", e.what());
+    }
     return 0;
 }
